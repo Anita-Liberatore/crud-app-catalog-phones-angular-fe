67da8c0f9e1a688989c2ce2e72b35b54175902f7
Anita Liberatore <anitaliberatore@hotmail.com>
2022-09-28 17:13:00 +0200

    add method add,delete,edit and reset

--- src/app/features/catalog/catalog.component.ts
+++ src/app/features/catalog/catalog.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Device } from 'src/app/model/device';
+import { NgForm } from '@angular/forms';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-catalog',
@@ -10,6 +12,7 @@ import { Device } from 'src/app/model/device';
 export class CatalogComponent {
 
   devices: Device[] = [];
+  activeDevice: Device | null = null;
   
   constructor(private http: HttpClient) { 
     this.getAll();
@@ -25,5 +28,40 @@ export class CatalogComponent {
       const index = this.devices.findIndex(d => d.id === device.id)
       this.devices.splice(index,1)
     })
+  }
+
+  save(form: NgForm) {
+    if(this.activeDevice) {
+      return this.edit(form);
+    }
+    console.log(form.value)
+
+    this.add(form)
+  }
+
+  setActive(device: Device) {
+    this.activeDevice = device;
+  }
+
+  add(form: NgForm) {
+    this.http.post<Device>(`http://localhost:3000/devices/`, form.value)
+      .subscribe(result => {
+        this.devices.push(result);
+        this.activeDevice = null;
+        form.reset();
+      });
+  }
+
+  edit(form: NgForm) {
+    this.http.patch<Device>(`http://localhost:3000/devices/${this.activeDevice?.id}`, form.value)
+      .subscribe(result => {
+        const index = this.devices.findIndex(d => d.id === this.activeDevice?.id)
+        this.devices[index] = result;
+      });
+  }
+
+  reset(form: NgForm) {
+    this.activeDevice = null;
+    form.reset();
   }
 }
